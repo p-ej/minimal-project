eb09ae221491f4bfe93eae00d4fbc1edc9c80fbc
p-ej <show400035@naver.com>
2025-10-31 03:01:28 +0900

    modify deploy

--- src/main.ts
+++ src/main.ts
@@ -30,6 +30,6 @@ async function bootstrap() {
 
   const port = configService.get<number>('env.port') || 3000;
   await app.listen(port, '0.0.0.0');
-  console.log(`🚀 Application is running on: http://0.0.0.0:${port}`);
+  console.log(`Application is running on: http://0.0.0.0:${port}`);
 }
 bootstrap();
